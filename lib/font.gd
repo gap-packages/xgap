e9338c7d2f8e28193bd6634fb98a66dee2e346af
GAP developer <gap@gap-system.org>
1999-06-30 09:13:03 +0000

    Some documentation issues: New example of a space group. This and that.

--- lib/font.gd
+++ lib/font.gd
@@ -2,14 +2,14 @@
 ##
 #W  font.gd                 	XGAP library                  Max Neunhoeffer
 ##
-#H  @(#)$Id: font.gd,v 1.1 1998/11/27 14:50:48 ahulpke Exp $
+#H  @(#)$Id: font.gd,v 1.2 1999/06/30 09:13:03 gap Exp $
 ##
 #Y  Copyright 1995-1997,  Lehrstuhl D fuer Mathematik,  RWTH Aachen,  Germany
 #Y  Copyright 1997,       Frank Celler,                 Huerth,       Germany
 #Y  Copyright 1998,       Max Neunhoeffer,              Aachen,       Germany
 ##
 Revision.pkg_xgap_lib_font_gd :=
-    "@(#)$Id: font.gd,v 1.1 1998/11/27 14:50:48 ahulpke Exp $";
+    "@(#)$Id: font.gd,v 1.2 1999/06/30 09:13:03 gap Exp $";
 
 
 #############################################################################
@@ -22,6 +22,14 @@ DeclareCategory( "IsFont", IsObject );
 #############################################################################
 ##
 #O  FontInfo( <font> )  . . . . . . . . . . . . . . . . . font info of a font
+##
+##  Returns the information about the font <font>. The result is a triple
+##  of integers. The first number is the maximal size
+##  of a character above the baseline in pixels, the second is the maximal size
+##  of a character below the baseline in pixels, and the third is the width
+##  in pixels of *all* characters, because it is always assumed, that the
+##  fonts are non-proportional. Use this function rather than accessing
+##  the component `fontInfo' of a font object directly!
 ##
 DeclareOperation( "FontInfo", [ IsFont ] );
 
